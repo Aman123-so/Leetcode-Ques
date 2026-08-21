fae98e6e0d1ed04d0d85ba55efadba79bcc84417
Aman Gupta <ammugupta893@gmail.com>
2026-08-22 01:02:37 +0530

    Time: 0 ms (100%), Space: 13.2 MB (52.5%) - LeetHub

---
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double trimMean(vector<int>& arr) {
+       sort(arr.begin(), arr.end());
+       int n= arr.size();
+       int remove = n/20;
+       int sum=0;
+       for(int i= remove ; i<n-remove; i++){
+        sum += arr[i];
+       }
+        int count= n-2*remove;
+        return double(sum)/count;
+        
+       
+       
+    }
+};
